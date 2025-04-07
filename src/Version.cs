29cd33ff033ed8e0b7b56a138136efb9525d5611
Kalle <kalle@kandru.de>
2025-04-07 18:27:47 +0200

    bump version to 0.1.1

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace ChatSounds
 {
     public partial class ChatSounds
     {
-        public override string ModuleVersion => "0.1.0";
+        public override string ModuleVersion => "0.1.1";
     }
 }

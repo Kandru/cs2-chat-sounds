d29db6d71493e60fd6b48577f828a58340d19381
derkalle4 <kalle@kandru.de>
2026-04-23 16:03:53 +0200

    bump version to 26.04.1

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace ChatSounds
 {
     public partial class ChatSounds
     {
-        public override string ModuleVersion => "0.1.3";
+        public override string ModuleVersion => "26.04.1";
     }
 }
